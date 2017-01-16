5a2ad2eb801ef197ab60e6fa3ce4d70f342fc4b8
Gowdhaman Jayaseelan <gowdhaman_j@yahoo.com>
2017-01-16 21:21:45 +0530

    Compiled my code

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -32,7 +32,7 @@ type SimpleChaincode struct {
 // ============================================================================================================================
 func main() {
 	err := shim.Start(new(SimpleChaincode))
-	if err != nil {
+	if err != nil {   
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
 }
